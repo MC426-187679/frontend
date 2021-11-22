631582d0a25b2bbb989ea206d30e6b734ae54232
Tiago de Paula <tiagodepalves@gmail.com>
2021-11-22 07:06:51 -0300

    Set up mock for the useMediaQuery hook and remove the ThemeProvider render

---
+++ src/config/themes.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+import { ThemeOptions, createTheme, lighten, responsiveFontSizes } from '@mui/material/styles'
+import { indigo, pink } from '@mui/material/colors'
+import { ptBR } from '@mui/material/locale'
+import { deepmerge } from '@mui/utils'
+
+/** Opções de tema. */
+export namespace Themes {
+    /**
+     * Configurações base os temas usados.
+     *
+     * @see https://bareynol.github.io/mui-theme-creator/
+     * @see https://material.io/design/color/the-color-system.html Nomes para as cores.
+     */
+    const baseTheme: ThemeOptions = {
+        palette: {
+            primary: {
+                main: indigo['500'],
+            },
+            secondary: {
+                main: pink['A400'],
+            },
+        },
+        components: {
+            MuiAutocomplete: {
+                styleOverrides: {
+                    inputRoot: {
+                        flexWrap: 'nowrap',
+                    },
+                },
+            },
+            MuiUseMediaQuery: {
+                defaultProps: {
+                    noSsr: true,
+                },
+            },
+        },
+    }
+
+    /** Constrói tema a partir do tema base. */
+    function buildTheme(options: ThemeOptions) {
+        const merged = deepmerge(baseTheme, options)
+        return responsiveFontSizes(createTheme(merged, ptBR))
+    }
+
+    /** Opção de tema claro. */
+    export const light = buildTheme({
+        palette: {
+            mode: 'light',
+            background: {
+                default: lighten(indigo['500'], 0.9),
+            },
+        },
+    })
+
+    /** Opção de tema escuro. */
+    export const dark = buildTheme({
+        palette: {
+            mode: 'dark',
+        },
+    })
+}
--- src/providers/Theme.tsx
+++ src/providers/Theme.tsx
@@ -1,75 +1,9 @@
-/* eslint-disable @typescript-eslint/dot-notation */
 import React, { Dispatch, ReactNode, createContext, useContext, useMemo } from 'react'
 import { useMediaQuery } from '@mui/material'
-import {
-    ThemeProvider as MuiThemeProvider,
-    ThemeOptions,
-    createTheme,
-    lighten,
-    responsiveFontSizes,
-} from '@mui/material/styles'
-import { indigo, pink } from '@mui/material/colors'
-import { ptBR } from '@mui/material/locale'
-import { deepmerge } from '@mui/utils'
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 
+import { Themes } from 'config/themes'
 import { useStorage } from 'hooks/useStorage'
-
-/** Opções de tema. */
-namespace Themes {
-    /**
-     * Configurações base os temas usados.
-     *
-     * @see https://bareynol.github.io/mui-theme-creator/
-     * @see https://material.io/design/color/the-color-system.html Nomes para as cores.
-     */
-    const baseTheme: ThemeOptions = {
-        palette: {
-            primary: {
-                main: indigo['500'],
-            },
-            secondary: {
-                main: pink['A400'],
-            },
-        },
-        components: {
-            MuiAutocomplete: {
-                styleOverrides: {
-                    inputRoot: {
-                        flexWrap: 'nowrap',
-                    },
-                },
-            },
-            MuiUseMediaQuery: {
-                defaultProps: {
-                    noSsr: true,
-                },
-            },
-        },
-    }
-
-    /** Constrói tema a partir do tema base. */
-    function buildTheme(options: ThemeOptions) {
-        const merged = deepmerge(baseTheme, options)
-        return responsiveFontSizes(createTheme(merged, ptBR))
-    }
-
-    /** Opção de tema claro. */
-    export const light = buildTheme({
-        palette: {
-            mode: 'light',
-            background: {
-                default: lighten(indigo['500'], 0.9),
-            },
-        },
-    })
-
-    /** Opção de tema escuro. */
-    export const dark = buildTheme({
-        palette: {
-            mode: 'dark',
-        },
-    })
-}
 
 /** Modos de tema, claro ou escuro. */
 export type ThemeMode = keyof typeof Themes
